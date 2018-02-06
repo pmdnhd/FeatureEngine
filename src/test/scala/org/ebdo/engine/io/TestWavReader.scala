cbc8ae5d41c1480c12b56a56a13eafe9b845ec72
Joseph Allemandou <joseph.allemandou@gmail.com>
2018-02-06 15:08:54 +0100

    Add WavReader class and tests (#5)
    WavReader class reads normalized double values in chunks from
    a wav file.

---
+++ src/test/scala/org/ebdo/engine/io/TestWavReader.scala
@@ -0,0 +1,109 @@
+package org.ebdo.engine.io
+
+import java.io.{File, FileInputStream, InputStream}
+import java.net.URL
+import javax.sound.sampled.{AudioFileFormat, AudioFormat, AudioInputStream, AudioSystem}
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.duration
+import scala.io.Source
+
+class TestWavReader extends FlatSpec with Matchers {
+
+  def rmse(s1: Seq[Double],s2: Seq[Double]): Double = {
+    assert(s1.length == s2.length)
+    val errs = s1.zip(s2).map { case (v1, v2) => math.pow(v1-v2, 2) }
+    val mse = errs.sum / errs.length
+    math.sqrt(mse)
+  }
+
+  val soundFilePath1 = "/wav/sin_16kHz_2.5s.wav"
+  val maxRMSEDiff = 0.001
+  val expectedFirstWave = Seq(
+    0.0d,
+    0.3826904296875d,
+    0.707122802734375d,
+    0.923858642578125d,
+    0.999969482421875d,
+    0.92388916015625d,
+    0.707061767578125d,
+    0.382781982421875d,
+    -9.1552734375E-5d,
+    -0.38262939453125d,
+    -0.707122802734375d,
+    -0.923919677734375d,
+    -0.99993896484375d,
+    -0.9239501953125d,
+    -0.70703125d,
+    -0.382720947265625d
+  )
+
+
+  "TestWavReader" should "Correctly read a example file" in {
+    val file: File = new File(getClass.getResource(soundFilePath1).toURI)
+    val wavReader = new WavReader(file)
+
+    val chunkSize = 16 // Length of sin signal without repetition
+
+    val chunks = wavReader.readChunks(chunkSize, 0)
+    chunks.size should equal(math.ceil(2.5 * 16000 / chunkSize))
+    chunks(0).size should equal(1) // single channel
+
+    // Chek the first wave of sin signal
+    val firstWave = chunks(0)(0).toSeq
+    rmse(firstWave, expectedFirstWave) should be < maxRMSEDiff
+
+    // Use repetitve aspect of signal to check reading correctness
+    Range.Int(1, math.floor(2.5 * 16000 / chunkSize).toInt, 1).foreach((chunkIdx: Int) => {
+      rmse(firstWave, chunks(chunkIdx)(0).toSeq) should be < maxRMSEDiff
+    })
+
+  }
+
+  "TestWavReader" should "Correctly read an example file partially" in {
+    val file: File = new File(getClass.getResource(soundFilePath1).toURI)
+    val wavReader = new WavReader(file)
+
+    val chunkSize = 16 // Length of sin signal without repetition
+    val nbChunks = 10
+
+    val chunks = wavReader.readChunks(chunkSize, 0, nbChunks)
+    chunks.size should equal(nbChunks)
+    chunks(0).size should equal(1) // single channel
+
+    // Chek the first wave of sin signal
+    val firstWave = chunks(0)(0).toSeq
+    rmse(firstWave, expectedFirstWave) should be < maxRMSEDiff
+
+    // Use repetitve aspect of signal to check reading correctness
+    Range.Int(1, nbChunks, 1).foreach((chunkIdx: Int) => {
+      rmse(firstWave, chunks(chunkIdx)(0).toSeq) should be < maxRMSEDiff
+    })
+
+  }
+
+  "TestWavReader" should "Correctly read an example file partially not from begining" in {
+    val file: File = new File(getClass.getResource(soundFilePath1).toURI)
+    val wavReader = new WavReader(file)
+
+    val chunkSize = 16 // Length of sin signal without repetition
+    val offset = 1024
+    val nbChunks = 10
+
+    val chunks = wavReader.readChunks(chunkSize, offset, nbChunks)
+    chunks.size should equal(nbChunks)
+    chunks(0).size should equal(1) // single channel
+
+    // Chek the first wave of sin signal
+    val firstWave = chunks(0)(0).toSeq
+    rmse(firstWave, expectedFirstWave) should be < maxRMSEDiff
+
+    // Use repetitve aspect of signal to check reading correctness
+    Range.Int(1, nbChunks, 1).foreach((chunkIdx: Int) => {
+      rmse(firstWave, chunks(chunkIdx)(0).toSeq) should be < maxRMSEDiff
+    })
+
+  }
+
+}

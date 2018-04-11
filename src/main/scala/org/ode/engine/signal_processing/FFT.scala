011959c2b80e9cddc36d5ac89c01b6d57281f4c2
Alex2242 <25385645+Alex2242@users.noreply.github.com>
2018-04-11 15:15:31 +0200

    Use onesided FFT in FFT class instead of twosided (#17)

--- src/main/scala/org/ode/engine/signal_processing/FFT.scala
+++ src/main/scala/org/ode/engine/signal_processing/FFT.scala
@@ -14,16 +14,15 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
-package org.ode.engine.signal_processing;
+package org.ode.engine.signal_processing
 
-import scala.math.{cos,Pi,pow,abs,sqrt}
-import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D
 
 /**
   * Wrapper class over edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D that 
   * computes FFT of nfft size over the signal of length nfft.
   * 
-  * Author: Paul Nguyen HD, Alexandre Degurse
+  * Author: Paul Nguyen HD, Alexandre Degurse, Joseph Allemandou
   *
   */
 
@@ -33,30 +32,38 @@ class FFT(nfft: Int) {
   // Instantiate the low level class that computes the fft
   val lowLevelFtt: DoubleFFT_1D = new DoubleFFT_1D(nfft)
 
+  val nfftEven: Boolean = nfft % 2 == 0
+
   /**
-  * Function that computes FFT for an Array
-  * The signal is zero-padded if needed (i.e. signal.length < nfft)
-  * An IllegalArgumentException is thrown if signal.length > nfft
-  * 
-  * Returns complex values represented by two consecutive Double, thus
-  * r(2*i) = Re(v_i) and r(2*i + 1) = Im(v_i) where r is the FFT over
-  * the signal and v_i the i'th complex value of the transformation
-  * 
-  * @param signal The signal to process as an Array[Double] of length nfft
-  * @return The FFT over the input signal as an Array[Double] of length 2*nfft
-  */
+    * Function that computes FFT for an Array
+    * The signal is zero-padded if needed (i.e. signal.length < nfft)
+    * An IllegalArgumentException is thrown if signal.length > nfft
+    *
+    * Returns complex values represented by two consecutive Double, thus
+    * r(2*i) = Re(v_i) and r(2*i + 1) = Im(v_i) where r is the FFT over
+    * the signal and v_i the i'th complex value of the transformation
+    *
+    * @param signal The signal to process as an Array[Double] of length nfft
+    * @return The FFT over the input signal as an Array[Double] of length nfft + (1 or 2)
+    *
+    */
   def compute(signal: Array[Double]) : Array[Double] = {
     if (signal.length > nfft) {
-      throw new IllegalArgumentException(s"Incorrect signal length (${signal.length}) for FFT (${nfft})")
+      throw new IllegalArgumentException(s"Incorrect signal length (${signal.length}) for FFT ($nfft)")
     }
 
-    // new value that contains the signal and padded with nfft zeros
-    // because the size doubles due to complex values
-    val fft: Array[Double] = signal ++ Array.fill(2*nfft - signal.length)(0.0)
+    // Result array containing the original signal
+    // followed by 0 up to nfft + (1 or 2) values
+    // depending of nfft parity (1 if odd, 2 if even)
+    val fft: Array[Double] = signal ++ Array.fill(nfft - signal.length + (if (nfftEven) 2 else 1))(0.0)
 
-    // // In place computation
-    lowLevelFtt.realForwardFull(fft)
+    // In place computation
+    lowLevelFtt.realForward(fft)
+    // Reordering of values to match regular layout
+    // of real followed by imaginary one
+    fft(nfft) = fft(1)
+    fft(1) = 0.0
 
-    return fft
+    fft
   }
 }
